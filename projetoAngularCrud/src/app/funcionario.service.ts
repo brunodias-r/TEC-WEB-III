03189436dbfa087ad9b1e66899d0901455c2d870
Bruno Rodrigues <bruno.brd73@gmail.com>
2022-06-21 13:28:37 -0300

    Projeto iniciado

---
+++ projetoAngularCrud/src/app/funcionario.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Funcionario } from './funcionario/funcionario.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FuncionarioService {
+
+  constructor() { }
+
+  listarTodos(): Funcionario[]{
+    const funcionarios = localStorage['funcionario'];
+    return funcionarios ? JSON.parse(funcionarios) : [];
+  }
+
+  cadastrar(funcionario: Funcionario):void{
+    const funcionarios = this.listarTodos();
+    funcionario.id = new Date().getTime();
+    funcionarios.push(funcionario);
+    localStorage['funcionario'] = JSON.stringify(funcionarios);
+  }
+
+  buscarPorId(id:number): Funcionario{
+    const funcionarios: Funcionario[] = this.listarTodos();
+    return funcionarios.find(funcionario => funcionario.id === id );
+  }
+
+  atualizar(funcionario: Funcionario): void{
+    const funcionarios: Funcionario[] = this.listarTodos();
+    funcionarios.forEach((func, pos, funcs) =>{
+      if(funcionario.id === func.id){
+        funcs[pos] = funcionario;
+      }
+    });
+    localStorage['funcionarios'] = JSON.stringify(funcionarios);
+  }
+}
